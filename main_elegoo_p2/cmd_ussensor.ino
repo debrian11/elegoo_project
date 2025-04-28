f03355a1874eda64a4f1da13914f301b439a9156
Brian Nguyen <debrian11@Brians-MacBook-Air.local>
2025-04-28 15:07:37 -0500

    changed initial main_elegoo to main_elgoo_p1 for phase 1. Created 2nd folder for p2 for phase 2 stuff

---
+++ main_elegoo_p2/cmd_ussensor.ino
@@ -0,0 +1,9 @@
+// -------- Ultrasonic sensor reading
+int read_us_sensor () {
+  if (current_time - ultrasonic_last_update >= 50) {
+    ultrasonic_last_update = current_time;
+    int distance_cm = sonar.ping_cm();
+    return distance_cm * 0.393701;
+  }
+}
+
---
+++ main_elegoo_p2/main_elegoo.ino
@@ -0,0 +1,61 @@
+/*
+  Use the elegoo board and shield
+*/
+unsigned long current_time = 0;
+unsigned long print_update;
+// --------Ultrasonic setup ------------- //
+#include <NewPing.h>
+#define echoPin 12
+#define trigPin 13
+#define max_dist 300 // max distance to measure in cm
+NewPing sonar(trigPin, echoPin, max_dist);
+unsigned long ultrasonic_last_update = 0;
+
+// --------Servo setup ------------- //
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+#define servo1_pin 10
+#define servo2_pin 11
+int servo1_angle = 0;
+int servo2_angle = 0;
+unsigned long servo1_lastUpdate = 0;
+unsigned long servo2_lastUpdate = 0;
+
+// --------Motor Setup ------------- //
+#define PWMA 5    // Right motors speed
+#define AIN1 7    // Right motors direction
+#define PWMB 6    // Left motors speed
+#define BIN1 8    // Left motors direction
+#define STBY 3    // Standby control pin
+enum motorstate_t { STOP, FORWARD, BACKWARD };
+motorstate_t currentmotorstate = STOP;
+
+void setup()
+{
+  Serial.begin(9600);
+  setupMotors();
+  setupServo();
+}
+
+void loop() {
+  current_time = millis();
+  int distance_in = read_us_sensor(); // Read Ultrasonic sensor and covert to inches
+  printOutput(distance_in); // print output of ultrasonic sensor
+
+  serial_check();
+
+  // 2. State Machine
+  switch(currentmotorstate) {
+      case FORWARD:
+          move_fwd();
+          break;
+      case BACKWARD:
+          move_back();
+          break;
+      case STOP:
+      default:
+          stop_motors();
+          break;
+  }
+}
